7b0dc1d7676dd79e6d649470f0398c6c9b3ddb39
leslie <alvarado.leslie12@yahoo.com>
2023-05-03 15:49:22 -0700

    lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your code here!
+/*
+const elements = document.createElement('div');
+
+document.body.append(elements);
+
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+    const li = document.createElement('li');
+    li.textContent = (i + 1).toString();
+    ul.append(li);
+}
+
+elements.append(ul);
+
+const element = document.getElementById('main');
+
+element.style.height = "300px"
+
+element.style.backgroundColor = "#27647B"
+
+element.textContent = "You've changed what's on the screen!"
+
+element.style.fontSize = "24px";
+
+element.style.marginLeft = "30px";
+
+element.style.lineHeight = 2;
+
+element.className = "pet-listing dog";
+
+element.classList.remove('dog');
+
+element.classList.add('cat','sale');
+*/
+
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "leslie is the champion"
